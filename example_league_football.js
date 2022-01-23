4a2502c53babac0e93febe7396f3d1f11b055a7a
Jheyson <jheremy26@gmail.com>
2022-01-23 10:16:10 -0500

    DONE : mostrar equipos por consola

---
+++ example_league_football.js
@@ -0,0 +1,9 @@
+// TODO: Mostrar los equipos inscritos por pantalla.
+
+const teams=["PSG","Milan","Real Madrid","Barcelona","Manchester City","Liverpool","Bayer Munich"]
+console.log(teams)
+// TODO: Crear la planificación de jornadas y partidos de cada jornada.
+// TODO: Mostrarla por pantalla.
+// TODO: Jugar lo partidos de todas las jornadas. Una vez terminada cada jornada, se deberá mostrar cómo queda la clasificación de la misma.
+// TODO: Una vez terminada la liga, se mostrarán estádísticas de número de goles totales y total de puntos ganados.
+
