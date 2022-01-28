0738185068e78ace40df0773ceeec7a96ece2c68
Jheyson <jheremy26@gmail.com>
2022-01-28 09:34:20 -0500

    herencia en javascript

--- example_league_football.js
+++ example_league_football.js
@@ -24,10 +24,31 @@ console.log(teams)
         this.matches=[]
 
     }
+    showTeams(){
+        console.log(this.teams)
+    }
  }
 
 
+ let anyLeague= new League("superliga",teams)
 
+ console.log(anyLeague.name)
+ anyLeague.showTeams()
+
+ let otherLeague=new League('Conference',["a","b","c"])
+
+ console.log(otherLeague.name)
+ otherLeague.showTeams()
+
+ class FootballLeague extends League {
+     constructor(name,teams,rounds=1){
+        super(name,teams,rounds)
+     }
+ }
+
+ let ChampionsLeague=new FootballLeague("Champions League",teams)
+ console.log(ChampionsLeague.name)
+ ChampionsLeague.showTeams()
 
 // TODO: Mostrarla por pantalla.
 // TODO: Jugar lo partidos de todas las jornadas. Una vez terminada cada jornada, se deberá mostrar cómo queda la clasificación de la misma.
