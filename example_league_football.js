211ceb8202fb3e512c1fc83e3208ee32a14c259e
Jheyson <jheremy26@gmail.com>
2022-02-11 10:35:10 -0500

    importo variable y clases de otro modulo y arreglo la salida principal de la propiedades

--- example_league_football.js
+++ example_league_football.js
@@ -1,48 +1,47 @@
 // TODO: Mostrar los equipos inscritos por pantalla.
 
-import {teams} from './teams.js';
-import {League} from './classes/League.js'
-import {FootballLeague} from './classes/FootballLeague.js'
+import { teams } from "./teams.js";
+import { League } from "./classes/League.js";
+import { FootballLeague } from "./classes/FootballLeague.js";
 
-console.log(teams)
+console.log(teams);
 
+let anyLeague = new League("superliga", teams);
 
- 
+console.log(anyLeague.name);
 
+let otherLeague = new League("Conference", ["a", "b", "c"]);
 
- let anyLeague= new League("superliga",teams)
+console.log(otherLeague.name);
 
- console.log(anyLeague.name)
- 
+const footballLeagueConfig = {
+  pointsPerWin: 3,
+  pointsPerDraw: 1,
+  pointsPerLose: 0,
+};
 
- let otherLeague=new League('Conference',["a","b","c"])
+let ChampionsLeague = new FootballLeague("Champions League", [
+  "A",
+  "B",
+  "C",
+  "D"
+]);
 
- console.log(otherLeague.name)
- 
+console.log('footballLeague.matchDaySchedule',ChampionsLeague.matchDaySchedule);
 
- 
-
- const footballLeagueConfig={
-    pointsPerWin:3,
-    pointsPerDraw:1,
-    pointsPerLose:0,
- }
-
- let ChampionsLeague=new FootballLeague("Champions League",['A','B','C','D'])
- console.log(ChampionsLeague)
+ChampionsLeague.scheduleMatchDays();
 
 // DONE: Mostrar los equipos  por pantalla.
 
-const teamNames=ChampionsLeague.teams.map(team=>team.name);
+const teamNames = ChampionsLeague.teams.map((team) => team.name);
 
-
-teamNames.forEach(team => {
-   console.log(team)
+teamNames.forEach((team) => {
+  console.log(team);
 });
 
-ChampionsLeague.scheduleMatchDays()
+
+console.log(ChampionsLeague.matchDaySchedule);
 
 // TODO: Mostrarla la planificacion  por pantalla.
 // TODO: Jugar lo partidos de todas las jornadas. Una vez terminada cada jornada, se deberá mostrar cómo queda la clasificación de la misma.
 // TODO: Una vez terminada la liga, se mostrarán estádísticas de número de goles totales y total de puntos ganados.
-
