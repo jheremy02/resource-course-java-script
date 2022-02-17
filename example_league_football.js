532da53ed0dc98251a38070dca1b34ec518acaa1
Jheyson <jheremy26@gmail.com>
2022-02-17 11:38:00 -0500

    añadimos soporte a la vista de las jorandas para ligas con equipos impares

--- example_league_football.js
+++ example_league_football.js
@@ -27,5 +27,22 @@ teamNames.forEach((team) => {
 console.log(ChampionsLeague.matchDaySchedule);
 
 // TODO: Mostrarla la planificacion  por pantalla.
+
+ChampionsLeague.matchDaySchedule.forEach((matchDay,matchDayIndex)=>{
+  console.log(`JORNADA ${matchDayIndex+1}`)
+  matchDay.forEach(match=>{
+
+    if (match.home==null || match.away ===null){
+        const teamName=match.home || match.away;
+        console.log(`${teamName} DESCANSA`)
+    } else {
+      console.log(`${match.home} vs ${match.away}`)
+    }
+    
+  })
+
+  console.log('==================================')
+})
+
 // TODO: Jugar lo partidos de todas las jornadas. Una vez terminada cada jornada, se deberá mostrar cómo queda la clasificación de la misma.
 // TODO: Una vez terminada la liga, se mostrarán estádísticas de número de goles totales y total de puntos ganados.
